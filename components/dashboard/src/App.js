2b6e8e5edee9f2631ff4df9f081b8efc0e3f28cb
Devin Brown <devin.g.brown@gmail.com>
2018-11-04 11:39:10 -0700

    brute force: running frontend in docker w/ nginx

--- components/dashboard/src/App.js
+++ components/dashboard/src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     <div className="App">
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
-        Click me
+        Click me!
       </button>
     </div>
   )
